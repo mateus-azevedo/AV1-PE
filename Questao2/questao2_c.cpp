df58406a98f0a32365ab90fe2a6882ae5e119be5
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-15 19:46:00 -0300

    testando a saida da matriz

--- Questao2/questao2_c.cpp
+++ Questao2/questao2_c.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
+    int matriz[3][3] = {{1,2,20},{-8,2,10},{5,10,3}};
+
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
+            //(j != 2) ? cout<<matriz[i][j]<<" " : cout<<matriz[i][j]<<endl;
+            (j != 2) ? printf("%2d  ", matriz[i][j]) : printf("%2d\n", matriz[i][j]);
+    
     return 0;
 }
