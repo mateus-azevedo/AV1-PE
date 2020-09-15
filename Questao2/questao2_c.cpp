5aca48f6e6ae3e32818b34d4580a7cfb2d2ddd35
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-15 19:50:05 -0300

    ajustando os valores da matriz

--- Questao2/questao2_c.cpp
+++ Questao2/questao2_c.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int matriz[3][3] = {{1,2,20},{-8,2,10},{5,10,3}};
+    int matriz[3][3] = {{1, -8, 5},{2, 2, 10},{20, 10, 3}};
 
     for(int i = 0; i < 3; i++)
         for(int j = 0; j < 3; j++)
