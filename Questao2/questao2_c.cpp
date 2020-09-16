db19f1e33c06e3094cef22efdf176b41f9d2a3ee
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-16 12:31:37 -0300

    implementado a soma das colunas

--- Questao2/questao2_c.cpp
+++ Questao2/questao2_c.cpp
@@ -4,12 +4,21 @@ using namespace std;
 
 int main()
 {
-    int matriz[3][3] = {{1, -8, 5},{2, 2, 10},{20, 10, 3}};
+    int matriz[3][3] = {{1, -8, 5},{2, 2, 10},{20, 10, 3}}, soma[3] = {0, 0, 0};
 
     for(int i = 0; i < 3; i++)
         for(int j = 0; j < 3; j++)
-            //(j != 2) ? cout<<matriz[i][j]<<" " : cout<<matriz[i][j]<<endl;
             (j != 2) ? printf("%2d  ", matriz[i][j]) : printf("%2d\n", matriz[i][j]);
+
+    cout<<"\nTESTANDO\nSoma das Colunas\n"<<endl;
+
+    for(int j = 0; j < 3; j++)
+        for(int i = 0; i < 3; i++)
+            soma[j] += matriz[i][j];
+
+    for(int j = 0; j < 3; j++)
+        for(int i = 0; i < 3; i++)
+            (i != 2) ? printf("%2d + ", matriz[i][j]) : printf("%2d = %2d\n", matriz[i][j], soma[j]);
     
     return 0;
 }
