8d615663001c32bfcd522b2dd207742e3fae7e95
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-16 12:39:38 -0300

    versão de teste

--- Questao2/questao2_c.cpp
+++ Questao2/questao2_c.cpp
@@ -6,11 +6,13 @@ int main()
 {
     int matriz[3][3] = {{1, -8, 5},{2, 2, 10},{20, 10, 3}}, soma[3] = {0, 0, 0};
 
+    cout<<"MATRIZ 3 x 3\n"<<endl;
+
     for(int i = 0; i < 3; i++)
         for(int j = 0; j < 3; j++)
             (j != 2) ? printf("%2d  ", matriz[i][j]) : printf("%2d\n", matriz[i][j]);
 
-    cout<<"\nTESTANDO\nSoma das Colunas\n"<<endl;
+    cout<<"\nSOMA DAS COLUNAS\n"<<endl;
 
     for(int j = 0; j < 3; j++)
         for(int i = 0; i < 3; i++)
