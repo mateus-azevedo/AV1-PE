6afabfd4f22e35b55283e1c540c8931746591ce7
Vinícius Oliveira <vinitag190@gmail.com>
2020-09-19 13:17:53 -0300

    Update questao1c.cpp

--- questao1c.cpp
+++ questao1c.cpp
@@ -6,26 +6,16 @@ using namespace std;
 /*Questão 1 - c) Escreva uma função recursiva para apresentar a soma de todos os números inteiros pares de zero até um número informado pelo usuário. Por exemplo: Para n = 7 a função deve retornar 0 + 2 + 4 + 6 = 12.*/
 
 int soma(int n){
-    //controle de parada
-    if(n==0){
+    if(n==0)                                    //controle de parada
         return n; 
-    }
-        
-    else{
-        //soma se o valor for par
-        if(n%2==0){
-            return (n + soma(n-2)); 
-        }
-        //soma se o valor for ímpar
-        else if(n%2==1){
-            return (n-1 + soma(n-3)); 
-        }
-    }
+    else if(n%2==0)                             //soma se o valor for par
+        return (n + soma(n-2));
+    else                                        //soma se o valor for ímpar
+        return (n-1 + soma(n-3)); 
 }
 
 main(){
     int num;
-
     cout<<"Insira um valor inteiro: "; cin>>num;
     cout<<endl;
     cout<<"Soma dos valores pares a partir do 0 ate o valor informado: "<<soma(num);
