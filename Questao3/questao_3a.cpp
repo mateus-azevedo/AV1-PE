9bc95d29429f82fb3a8b91739956c7ef5f35066b
Vinícius Oliveira <vinitag190@gmail.com>
2020-09-22 15:41:15 -0300

    Estética e finalização do programa

--- Questao3/questao_3a.cpp
+++ Questao3/questao_3a.cpp
@@ -10,10 +10,15 @@ main(){
     float matriz[3][3];
     int i, j;
 
+    cout<<"          ENDERECO DE CADA POSICAO DA MATRIZ";
     for(i=0; i<3; i++){
-        cout<<endl;
+        cout<<endl<<endl;
         for(j=0; j<3; j++){
-            cout<<"["<<i<<"]["<<j<<"]: "<<&matriz[i][j];
+            cout<<"["<<i<<"]["<<j<<"]: "<<&matriz[i][j]<<"    ";
         }
     }
+
+    cout<<endl<<endl;
+    system("pause");
+    return 0;
 }
