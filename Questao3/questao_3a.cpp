f45024b6543ca88712ddfa8e193a582e990a2e0a
Vinícius Oliveira <vinitag190@gmail.com>
2020-09-22 15:22:58 -0300

    Criação da main e das variáveis

--- Questao3/questao_3a.cpp
+++ Questao3/questao_3a.cpp
@@ -5,3 +5,10 @@
 using namespace std;
 
 /*Questão 3a) Crie um programa que contenha uma matriz de float contendo 3 linhas e 3 colunas. Imprima o endereço de cada posição dessa matriz*/
+
+main(){
+    float matriz[3][3];
+    int i, j;
+
+    
+}
