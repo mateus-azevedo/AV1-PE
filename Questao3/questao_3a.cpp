f3f9191ebe6f86c555f67a4234892d05639acb67
Vinícius Oliveira <vinitag190@gmail.com>
2020-09-22 15:18:23 -0300

    Inclusão das bibliotecas e enunciado da questão

---
+++ Questao3/questao_3a.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+
+using namespace std;
+
+/*Questão 3a) Crie um programa que contenha uma matriz de float contendo 3 linhas e 3 colunas. Imprima o endereço de cada posição dessa matriz*/
