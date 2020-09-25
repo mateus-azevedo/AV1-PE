29d309c7703152ed170986a048ee99b6abf7abd3
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-25 13:47:25 -0300

    questao finalizada

--- Questao2/questao2c.cpp
+++ Questao2/questao2c.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
-    int matriz[3][3] = {{1, -8, 5},{2, 2, 10},{20, 10, 3}}, soma[3] = {0, 0, 0};
+    int matriz[3][3] = {{1, -8, 5},{2, 2, 10},{20, 10, 3}}, soma[3] = {0, 0, 0}; 
+    //PARA EXECUTAR O EXEMPLO DO EXERCÍCIO COMENTE DA LINHA 10 ~ LINHA 14 E EXIBIRÁ O VALOR DO VETOR DO EXEMPLO
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+        {
+            cout<<"Entre com o valor da Matriz["<<i<<"]["<<j<<"]: "; cin>>matriz[i][j];
+        }
+    
+    system("cls");
 
     cout<<"MATRIZ 3 x 3\n"<<endl;
 
