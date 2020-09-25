8b7b00e20ec973c5f9e0bbd57c847ce254e1702b
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-25 13:23:29 -0300

    adicionado função do tipo boolean

--- Questao1/questao1_b.cpp
+++ Questao1/questao1_b.cpp
@@ -1,31 +1,33 @@
 #include<iostream>
 #include<stdlib.h>
+#include<stdbool.h>
+
 using namespace std;
 
-int verificaPrimo(int x)
+bool verificaPrimo(int x)
 {
     if ((x % 2) == 0)
     {
         cout<<x<<" e divisivel por 2"<<endl;
-        return 0;
+        return false;
     }
     else if ((x % 3) == 0)
     {
         cout<<x<<" e divisivel por 3"<<endl;
-        return 0;
+        return false;
     }
     else if ((x % 5) == 0)
     {
         cout<<x<<" e divisivel por 5"<<endl;
-        return 0;
+        return false;
     }
     else if ((x % 7) == 0)
     {
         cout<<x<<" e divisivel por 7"<<endl;
-        return 0;
+        return false;
     }
     else
-        return 1;
+        return true;
 }
 
 int main()
@@ -34,7 +36,7 @@ int main()
 
     cout<<"Numero: "; cin>>num;
 
-    if (verificaPrimo(num) == 1 )
+    if (verificaPrimo(num) == true )
         cout<<"Verdadeiro";
     else
         cout<<"Falso";
