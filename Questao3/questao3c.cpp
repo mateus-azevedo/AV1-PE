d189dbf2644ad7b97d4600c9075027409b00d597
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-26 21:55:15 -0300

    estrutura básica da questão

--- Questao3/questao3c.cpp
+++ Questao3/questao3c.cpp
@@ -1,10 +1,26 @@
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
+#define MAX 3
+
+int multiplicacao(int x, int y)
+{
+    return x * y;
+}
 
 int main()
 {
-    cout<<"Iniciando Questao 3.C";
+    int num1[MAX], num2[MAX], resultado[MAX];
+
+    cout<<"\tENTRE COM OS VALORES:"<<endl;
+    
+    for (int i = 0; i < MAX; i++)
+    {
+        cout<<"\nMultiplicando ("<<i+1<<"): "; cin>>num1[i];
+        cout<<"Multiplicador ("<<i+1<<"): "; cin>>num2[i];
+
+        resultado[i] = multiplicacao(num1[i], num2[i]);
+    }
     
     return 0;
 }
