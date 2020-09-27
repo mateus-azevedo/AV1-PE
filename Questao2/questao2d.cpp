4409a636fb3c4c816e2be42fece8b26d352b5e7b
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-26 21:03:41 -0300

    Questão Finalizada(?)

--- Questao2/questao2d.cpp
+++ Questao2/questao2d.cpp
@@ -3,21 +3,9 @@
 using namespace std;
 #define TOTALUNO 5
 
-/*
-    São 5 alunos, ou seja, 5 linhas no meu vetor
-
-    1. Coluna: número de matrícula (use um inteiro)
-    2. Coluna: Avaliação 1
-    3. Coluna: Avaliação 2
-    4. Coluna: Média dos trabalhos
-    5. Coluna: Nota final
-
-    Ler as 4 primeiras colunas
-*/
-
 struct dadosaluno{
-    int matricula[TOTALUNO]={2015,2016,2017,2018,2019};
-    float av1[TOTALUNO], av2[TOTALUNO], mediaTrabalho[TOTALUNO], notaFinal[TOTALUNO] = {5,10,8,4,5};
+    int matricula[TOTALUNO];
+    float av1[TOTALUNO], av2[TOTALUNO], mediaTrabalho[TOTALUNO], notaFinal[TOTALUNO];
 };
 
 float notaFinal(float av1, float av2, float trab)
@@ -31,11 +19,11 @@ int main()
     
     for (int i = 0; i < TOTALUNO; i++)
     {
-        // cout<<"\nMatricula: "; cin>>info.matricula[i]; 
-        // cout<<"AV1: "; cin>>info.av1[i];
-        // cout<<"AV2: "; cin>>info.av2[i];
-        // cout<<"Media do Trabalho: "; cin>>info.mediaTrabalho[i];
-        // info.notaFinal[i] = notaFinal(info.av1[i], info.av2[i], info.mediaTrabalho[i]);
+        cout<<"\nMatricula: "; cin>>info.matricula[i]; 
+        cout<<"AV1: "; cin>>info.av1[i];
+        cout<<"AV2: "; cin>>info.av2[i];
+        cout<<"Media do Trabalho: "; cin>>info.mediaTrabalho[i];
+        info.notaFinal[i] = notaFinal(info.av1[i], info.av2[i], info.mediaTrabalho[i]);
     }
 
     system("cls");
@@ -56,7 +44,9 @@ int main()
     mediaNotasFinais = (mediaNotasFinais / TOTALUNO);
 
     cout<<"\t\tMELHOR ALUNO\n\tMATRICULA: "<<info.matricula[indice]<<"\n\tNOTA FINAL: "<<info.notaFinal[indice]<<endl<<endl;
-    cout<<"MEDIA ARITMETICA DAS NOTAS FINAIS DOS ALUNOS: "<<mediaNotasFinais;
+    cout<<"MEDIA ARITMETICA DAS NOTAS FINAIS DOS ALUNOS: "<<mediaNotasFinais<<endl<<endl;
+
+    system("pause");
 
     return 0;
 }
