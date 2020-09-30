be7d8c26cc1dce37e26b3ed28d17c3bfb1b3e78c
Vinícius Oliveira <vinitag190@gmail.com>
2020-09-30 15:01:13 -0300

    Criação das estruturas for para leitura

--- Questao2/questao2e.cpp
+++ Questao2/questao2e.cpp
@@ -8,5 +8,10 @@ main(){
     float matriz[3][6], somaPares, media;
     int i, j;
 
-    
+    for(i=0; i<3; i++){
+        for(j=0; j<6; j++){
+            cout<<"Insira um valor real para a posicao ["<<i<<"]["<<j<<"]: "; cin>>matriz[i][j];
+            cout<<endl;
+        }
+    }
 }
