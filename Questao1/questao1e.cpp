0073c15d25e1a43ebd1e537b0b5d19af459a4fb6
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-02 19:04:01 -0300

    estrutura básica da questão montada

--- Questao1/questao1e.cpp
+++ Questao1/questao1e.cpp
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 using namespace std;
 
+int maximoDivisorComum(float x, float y)
+{
+
+}
+
 int main()
 {
-  cout<<"HELLO WORLD, QUESTAO 1 - E";
+  float num1, num2;
+  int maiorDivisorComum;
+
+  maiorDivisorComum = maximoDivisorComum(num1, num2);
+  cout<<"O maior divisor: "<<maiorDivisorComum;
   
   return 0;
 }
