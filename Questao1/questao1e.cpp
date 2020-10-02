b6c7fa7dae46934ed57c16b7a585b5569de8e90e
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-02 19:27:26 -0300

    tentando organizar o pensamento

--- Questao1/questao1e.cpp
+++ Questao1/questao1e.cpp
@@ -9,7 +9,7 @@ int maximoDivisorComum(float x, float y)
 
 int main()
 {
-  float num1, num2;
+  float num1 = 18, num2 = 24;
   int maiorDivisorComum;
 
   maiorDivisorComum = maximoDivisorComum(num1, num2);
